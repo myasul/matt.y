446bea90d3862f2a84ffc81392c6d8766d02d2ac
Matthew Yasul <yasulmatthew@gmail.com>
2022-04-18 20:02:11 +0800

    Remove FB OG image meta tags

--- src/components/templates/blog/MetaTags.tsx
+++ src/components/templates/blog/MetaTags.tsx
@@ -8,6 +8,32 @@ import { getSrc } from 'gatsby-plugin-image'
 
 const toPost = compile('/:slug([a-zA-Z0-9-]+)/')
 
+
+// {/* TODO: Tags should be added here */}
+// {/* <meta property="article:tag" content="" key="" /> */}
+// {/* <meta property="og:image" content={imageUrl} />
+// <meta property="og:image:secure_url" content={imageUrl} />
+// <meta property="og:image:type" content="image/jpeg" />
+// <meta property="og:image:width" content="640" />
+// <meta property="og:image:height" content="360" />
+// <meta property="og:image:alt" content="Post cover" /> */}
+
+
+// {/* Meta properties to transform links in
+// the website as cards in twitter */}
+// <meta property="twitter:card" content="summary_large_image" />
+// <meta property="twitter:title" content={post.title} />
+// <meta property="twitter:description" content={post.description} />
+// <meta property="twitter:url" content={postUrl} />
+// <meta property="twitter:image" content={imageUrl} />
+// <meta property="twitter:label1" content="Written by" />
+// <meta property="twitter:data1" content={author.name} />
+// <meta property="twitter:label2" content="Filed under" />
+// {/* TODO: Tags should be added here */}
+// <meta property="twitter:data2" content="" />
+// {/* TODO: Tags should be added here */}
+// <meta property="twitter:creator" content={author.usernames.twitter} />
+
 export const MetaTags = ({ post }: { post: Post }) => {
     const { author, site } = useSiteMetadata()
 
@@ -29,31 +55,6 @@ export const MetaTags = ({ post }: { post: Post }) => {
             <meta property="og:url" content={postUrl} />
             <meta name="author" property="article:author" content={author.profiles.facebook} />
             <meta property="article:published_time" content={post.published} />
-
-            {/* TODO: Tags should be added here */}
-            {/* <meta property="article:tag" content="" key="" /> */}
-            <meta property="og:image" content={imageUrl} />
-            <meta property="og:image:secure_url" content={imageUrl} />
-            <meta property="og:image:type" content="image/jpeg" />
-            <meta property="og:image:width" content="640" />
-            <meta property="og:image:height" content="360" />
-            <meta property="og:image:alt" content="A shiny red apple with a bite taken out" />
-
-
-            {/* Meta properties to transform links in
-            the website as cards in twitter */}
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:title" content={post.title} />
-            <meta property="twitter:description" content={post.description} />
-            <meta property="twitter:url" content={postUrl} />
-            <meta property="twitter:image" content={imageUrl} />
-            <meta property="twitter:label1" content="Written by" />
-            <meta property="twitter:data1" content={author.name} />
-            <meta property="twitter:label2" content="Filed under" />
-            {/* TODO: Tags should be added here */}
-            <meta property="twitter:data2" content="" />
-            {/* TODO: Tags should be added here */}
-            <meta property="twitter:creator" content={author.usernames.twitter} />
         </Helmet>
     )
 }
