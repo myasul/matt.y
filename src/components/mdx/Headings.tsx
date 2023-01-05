610d4d2aaa4ba2969488727ff881fa49ea6ecfb1
Matthew Yasul <yasulmatthew@gmail.com>
2023-01-05 11:56:39 +0800

    Fix Paragraph spacing and add images

--- src/components/mdx/Headings.tsx
+++ src/components/mdx/Headings.tsx
@@ -8,25 +8,25 @@ export const Title = styled.h1`
 `
 
 export const SubTitle = styled.h2`
+    color: black;
+    margin: 3rem 0 0 0;
     text-align: left;
-    margin: 1rem 0;
-    font-weight: 400;
+    font-weight: bolder;
     font-size: 1.8rem;
-    margin-top: 4rem;
 `
 
 export const H3 = styled.h3`
+    color: black;
     font-size: 1.6rem;
-    font-weight: 300;
+    font-weight: bolder;
     text-align: left;
-    margin: auto 0;
-    margin-top: 3rem;
+    margin: 2rem 0 0 0;
 `
 
 export const H4 = styled.h4`
+    color: black;
     font-size: 1.4rem;
-    font-weight: 900;
+    font-weight: bolder;
     text-align: left;
-    margin: auto 0;
-    margin-top: 3rem;
+    margin: 2rem 0 0 0;
 `
--- src/components/mdx/Paragraph.tsx
+++ src/components/mdx/Paragraph.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 export const Paragraph = styled.p`
     text-align: left;
     margin: 0;
-    line-height: 2.5rem;
+    line-height: 1.8rem;
     font-size: 1.1rem;
     font-weight: 100;
-    margin-top: 1rem;
+    margin-top: 1.5rem;
 `
