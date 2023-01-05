9f0b5be919ad44eb41edf159966f05f8a227e2df
Matthew Yasul <yasulmatthew@gmail.com>
2023-01-05 17:17:22 +0800

    Fix website crash

--- src/lib/utils/DateUtil.ts
+++ src/lib/utils/DateUtil.ts
@@ -1,12 +1,12 @@
 export class DateUtil {
     public static toAbbrevDateTime (date: Date) {
-        const dateTimeRegex = /(?<=[\w]{3}[\s])(.+)/g
+        const dateTimeRegex = /(?:[\w]{3}[\s])(.+)/g
         const dateString = date.toDateString()
 
         const match = dateString.match(dateTimeRegex)
 
         if (!(match !== null && match.length > 0)) throw new Error(`Invalid date: ${dateString}`)
 
-        return match[0].replace(/(?<=\d)\s(?=\d)/g, ', ')
+        return match[0].replace(/(\d)\s(?=\d)/g, '$1, ')
     }
 }
