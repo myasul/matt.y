2b9fea30f17bb2040ef9b71ab397ee0c96acfcd9
Matthew Yasul <yasulmatthew@gmail.com>
2026-07-15 10:37:15 +1200

    feat: update context management blog

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -4,6 +4,7 @@ import { Title, H3, SubTitle, H4 } from './Headings'
 import { Paragraph } from './Paragraph'
 import { UnorderedList } from './UnorderedList'
 import { List } from './List'
+import { Strong } from './Strong'
 
 export default {
     h1: (props: any) => <Title {...props} />,
@@ -12,5 +13,6 @@ export default {
     h4: (props: any) => <H4 {...props} />,
     p: (props: any) => <Paragraph {...props} />,
     ul: (props: any) => <UnorderedList {...props} />,
-    li: (props: any) => <List {...props} />
+    li: (props: any) => <List {...props} />,
+    strong: (props: any) => <Strong {...props} />
 }
